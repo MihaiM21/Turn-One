4952542e65832e7258bb0b31e7efe08ab9beace3
Mihai <mihai.marinescu2112@yahoo.com>
2025-10-25 15:38:19 +0300

    small update

--- turn-one-client/next.config.ts
+++ turn-one-client/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'https://backend.t1f1.com/api',
+  }
 };
 
 export default nextConfig;
