5b2ded2fe6f75f4210e95bbeaf664517557eabae
Mihai <mihai.marinescu2112@yahoo.com>
2026-04-29 15:49:54 +0300

    chore: removed unused code

--- turn-one-client/lib/newsService.ts
+++ turn-one-client/lib/newsService.ts
@@ -1,8 +1,6 @@
 import { SessionDashboardData } from "@/types/news-types";
 import { fetchFromExternalAPI } from "@/lib/data-fetcher";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://apidev.t1f1.com";
-
 export async function getLatestSessionData(): Promise<SessionDashboardData> {
   return fetchFromExternalAPI(`v1/dashboard`);
 }
