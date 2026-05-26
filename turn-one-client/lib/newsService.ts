85a72c4cc3ef161b43cb0969ad3c53dc44b6c0d6
Mihai <mihai.marinescu2112@yahoo.com>
2026-05-26 22:49:37 +0300

    fix: use v2 instead of v1 for the latest session

--- turn-one-client/lib/newsService.ts
+++ turn-one-client/lib/newsService.ts
@@ -2,9 +2,9 @@ import { SessionDashboardData } from "@/types/news-types";
 import { fetchFromExternalAPI } from "@/lib/data-fetcher";
 
 export async function getLatestSessionData(): Promise<SessionDashboardData> {
-  return fetchFromExternalAPI(`v1/dashboard`);
+  return fetchFromExternalAPI(`v2/dashboard`);
 }
 
 export async function getLatestSessionDataClient(): Promise<SessionDashboardData> {
-  return fetchFromExternalAPI(`v1/dashboard`);
+  return fetchFromExternalAPI(`v2/dashboard`);
 }
